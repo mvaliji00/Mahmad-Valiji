a2ceda628e1429bf807051572411b00c0c21e552
mvaliji00 <mvaliji@laingorourke.com>
2020-07-08 21:50:08 +0100

    rest api implementation v2

--- flask_dataTable2/static/sidebarAnimation.js
+++ flask_dataTable2/static/sidebarAnimation.js
@@ -1,11 +1,12 @@
-function openNav() {
-  document.getElementById("sidebar").style.width = "250px";
-  document.getElementById("content").style.marginLeft = "250px";
+let dashboardVisible = false;
+
+function sidebarToggle() {
+    if (!dashboardVisible) {
+        document.getElementById("sidebar").style.width = "250px";
+        document.getElementById("content").style.marginLeft = "250px";
+    } else {
+        document.getElementById("sidebar").style.width = "0";
+        document.getElementById("content").style.marginLeft= "0";
+    }
+    dashboardVisible = !dashboardVisible;
 }
-
-function closeNav() {
-  document.getElementById("sidebar").style.width = "0";
-  document.getElementById("content").style.marginLeft= "0";
-}
-
-
